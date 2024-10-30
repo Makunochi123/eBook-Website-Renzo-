78a02b9adf95bdd6875b812972b5bb93c27cb829
unknown <rjc0399@gmail.com>
2024-10-30 10:01:56 +0800

    Shutting Down Temporarily.

---
+++ morethan.py
@@ -0,0 +1,67 @@
+a = 100
+b = 33
+if a > b :
+    print("a is greater than b")
+elif a==b:
+    print("a and b are equal")
+else:
+    print("a is greater than b")
+
+a = 50
+b = 33
+if a > b:
+    print("a is greater than b")
+elif a==b:
+    print("a and b are equal")
+else:
+    print("a is greater than b")
+
+
+a = 33
+b = 100
+if b > a:
+    print("B is greater than A.") 
+elif a==b:
+    print("Both A and B are equal.")   
+else:
+    print("B is greater than A.")
+
+a = 100
+b = 50
+if a > b:
+    print("A is more than B")
+elif a==b:
+    print("Both A and B are Equal.")
+else:
+    print("A is more than B.")
+
+a = 50
+b = 100
+if b > a:
+    print("B is more than A")
+elif a==b:
+    print("A and B is equal.")
+else:
+    print("B is more than A")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+a = 120
+b = 20
+if a > b:
+    print("A is > B")
+elif a==b:
+    print("Both sides are equal.")
+else:
+    print("A is > B")
